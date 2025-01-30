88fcc1c987b869d3a3155302b9023d9503ae53ea
Matt Kint <matt.kint@gmail.com>
2025-01-29 21:15:08 -0800

    second level--explosion basics

--- Levels/MainMenu/main_menu.gd
+++ Levels/MainMenu/main_menu.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const LEVEL_1 = preload("res://Levels/Level_1/level_1.tscn")
+const LEVEL_2 = preload("res://Levels/Level_2/level_2.tscn")
 
 
 func _on_quit_pressed() -> void:
@@ -8,4 +9,5 @@ func _on_quit_pressed() -> void:
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_packed(LEVEL_1)
+	#get_tree().change_scene_to_packed(LEVEL_1)
+	get_tree().change_scene_to_packed(LEVEL_2)
